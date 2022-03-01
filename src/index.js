d3aa160bcddb6771cbd204102c149ab73318bdb4
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-01 21:48:03 +0530

    Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './index.css';
+
 const list = document.querySelectorAll('.each-score');
 
 list.forEach((el, i) => {
-	if (i % 2 === 0) {
-		el.style.backgroundColor = 'white';
-	} else el.style.backgroundColor = 'lightgrey';
+  if (i % 2 === 0) {
+    el.style.backgroundColor = 'white';
+  } else el.style.backgroundColor = 'lightgrey';
 });
