84db802affc0bf7425a1eb469768ace1d6143dbb
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-01 21:39:43 +0530

    Added js file for changing background of list elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './index.css';
-console.log('Hello World');
+const list = document.querySelectorAll('.each-score');
+
+list.forEach((el, i) => {
+	if (i % 2 === 0) {
+		el.style.backgroundColor = 'white';
+	} else el.style.backgroundColor = 'lightgrey';
+});
