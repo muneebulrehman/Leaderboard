ee76564386438a6c33bd08f9ce3acc0843f3d6f2
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-02 19:01:33 +0530

    Fixed linter errors and updated readme file

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import './index.css';
 import { setScore, getScore } from './modules/api';
 import renderList from './modules/render';
 
-const list = document.querySelectorAll('.each-score');
 const nameEl = document.getElementById('name');
 const scoreEl = document.getElementById('score');
 const submit = document.querySelector('.submit');
 const refresh = document.querySelector('.refresh');
 
 submit.addEventListener('click', async () => {
-	const name = nameEl.value.trim();
-	const score = +scoreEl.value.trim();
-	if (name && score) {
-		await setScore({ user: name, score: score });
-		nameEl.value = '';
-		scoreEl.value = '';
-	}
+  const name = nameEl.value.trim();
+  const score = +scoreEl.value.trim();
+  if (name && score) {
+    await setScore({ user: name, score });
+    nameEl.value = '';
+    scoreEl.value = '';
+  }
 });
 
 const getList = async () => {
-	const data = await getScore();
-	renderList(data);
+  const data = await getScore();
+  renderList(data);
 };
 
 refresh.addEventListener('click', () => {
-	getList();
+  getList();
 });
+
 getList();
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,24 +1,16 @@
 const key = 'JUyd7IruRAoCCkTWXR1P';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores`;
-const setScore = async score => {
-	try {
-		await fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(score),
-			headers: { 'Content-type': 'application/json; charset=UTF-8' },
-		});
-	} catch (err) {
-		throw err;
-	}
+const setScore = async (score) => {
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(score),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
 };
 const getScore = async () => {
-	try {
-		const fetchData = await fetch(url);
-		const { result } = await fetchData.json();
-		return result;
-	} catch (err) {
-		throw err;
-	}
+  const fetchData = await fetch(url);
+  const { result } = await fetchData.json();
+  return result;
 };
 
 export { setScore, getScore };
