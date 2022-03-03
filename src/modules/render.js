9e87add7183f7e0e9a6b07177a6f11a6de3abbdd
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-03 18:20:03 +0530

    Changed styling

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,16 +1,16 @@
 const list = document.querySelector('.score-board');
 
-const renderList = (data) => {
-  list.innerHTML = '';
+const renderList = data => {
+	list.innerHTML = '';
 
-  if (data.length > 0) {
-    const newData = data.sort((a, b) => a.score - b.score);
-    newData.forEach((el, i) => {
-      const addClass = i % 2 === 0 ? 'white' : 'grey';
-      const html = `<li class="each-score ${addClass}"><span>${el.user}:</span><span>${el.score}</span></li>`;
-      list.insertAdjacentHTML('beforeend', html);
-    });
-  }
+	if (data.length > 0) {
+		const newData = data.sort((a, b) => b.score - a.score);
+		newData.forEach((el, i) => {
+			const addClass = i % 2 === 0 ? 'white' : 'grey';
+			const html = `<li class="each-score ${addClass}"><span>${el.user}:</span><span>${el.score}</span></li>`;
+			list.insertAdjacentHTML('beforeend', html);
+		});
+	}
 };
 
 export default renderList;
