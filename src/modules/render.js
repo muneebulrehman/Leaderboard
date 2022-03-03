577c5ce71467034236dd4e4a19c03e43f433e526
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-03 21:44:13 +0530

    Fixed linter errors

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,18 +1,20 @@
 const list = document.querySelector('.score-board');
 
-const renderList = data => {
-	list.innerHTML = '';
+const renderList = (data) => {
+  list.innerHTML = '';
 
-	if (data.length > 0) {
-		const newData = data.sort((a, b) => b.score - a.score);
-		newData.forEach((el, i) => {
+  if (data.length > 0) {
+    const newData = data.sort((a, b) => b.score - a.score);
+    newData.forEach((el, i) => {
+      /* eslint-disable */
 			const img = i === 0 ? '🥇' : i === 1 ? '🥈' : '🥉';
-			const imgIcon = i < 3 ? img : '';
-			const addClass = i % 2 === 0 ? 'white' : 'grey';
-			const html = `<li class="each-score ${addClass}"> <span>${el.user}:</span><span>${el.score}</span> ${imgIcon}</li>`;
-			list.insertAdjacentHTML('beforeend', html);
-		});
-	}
+			/* eslint-enable */
+      const imgIcon = i < 3 ? img : '';
+      const addClass = i % 2 === 0 ? 'white' : 'grey';
+      const html = `<li class="each-score ${addClass}"> <span>${el.user}:</span><span>${el.score}</span> ${imgIcon}</li>`;
+      list.insertAdjacentHTML('beforeend', html);
+    });
+  }
 };
 
 export default renderList;
--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	entry: './src/index.js',
-	devServer: {
-		static: './dist',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.html$/,
-				use: ['html-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				use: [
-					{
-						// loader: 'file-loader',
-						loader: 'url-loader',
-						options: {
-							limit: 5000,
-						},
-					},
-				],
-				type: 'asset/resource',
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			// favicon: './src/assets/images/favicon.ico',
-			template: './src/index.html',
-		}),
-	],
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            // loader: 'file-loader',
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+            },
+          },
+        ],
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // favicon: './src/assets/images/favicon.ico',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
