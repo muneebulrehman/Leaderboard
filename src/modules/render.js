4a49d23b7dff3c40a4dfa15c72e46f60c51ac8f9
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-03 19:02:16 +0530

    added some styling touches

--- src/modules/render.js
+++ src/modules/render.js
@@ -6,8 +6,10 @@ const renderList = data => {
 	if (data.length > 0) {
 		const newData = data.sort((a, b) => b.score - a.score);
 		newData.forEach((el, i) => {
+			const img = i === 0 ? 'gold.png' : i === 1 ? 'silver.png' : 'bronze.png';
+			const imgIcon = i < 3 ? `<img src="./images/${img}" class="icon" alt="">` : '';
 			const addClass = i % 2 === 0 ? 'white' : 'grey';
-			const html = `<li class="each-score ${addClass}"><span>${el.user}:</span><span>${el.score}</span></li>`;
+			const html = `<li class="each-score ${addClass}"><span>${el.user}:</span><span>${el.score}</span> ${imgIcon}</li>`;
 			list.insertAdjacentHTML('beforeend', html);
 		});
 	}
